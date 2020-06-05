155b69a7e9abf4b7bf0aa5e4b11a0961e26a4a15
Ernesto Gigliotti <ernestogigliotti@gmail.com>
2020-06-04 23:43:51 -0300

    cambio en comparacion de archivos por lineas. Fix comparacion del archivo con si mismo

--- main.py
+++ main.py
@@ -1,30 +1,32 @@
 import os
 import csv
+import sys
 from File import File
 from Group import Group
 
-Group.bytes_delta = 200 # seteo nivel de similitud a 200 bytes (aprox 200 caracteres)
+Group.same_lines_percent_level = 60 # Seteo porcentaje de lineas en comun entre archivos
+
+# Leo de los argumentos de la consola el directorio a analizar
+try:
+	analize_path = sys.argv[1]
+except Exception as e:
+	print("Path no specified. Using current directory...")
+	analize_path="."
+
 
 #cargo archivos a analizar
 filesToAnalize = []
-# falta cargar estos archivos barriendo los directorios de los proyectos con os.walk
-# files.append(File("archivo1.c",400))
-# files.append(File("archivo2.c",800))
-# files.append(File("archivo3.c",1500))
-# files.append(File("archivo4.c",6900))
-# files.append(File("archivo5.c",6850))
-# files.append(File("archivo6.c",1800))
-# files.append(File("archivo7.c",400))
-# files.append(File("archivo8.c",10654))
-# files.append(File("archivo9.c",15987))
-# files.append(File("archivo10.c",15977))
 
-for root, dirs, files in os.walk(".", topdown=False):
+for root, dirs, files in os.walk(analize_path, topdown=False):
 	for name in files:
 		if name.endswith(".c") and name != "specs.c" and name != "spec.c":
 			file_path = os.path.join(root, name)
 			file_stats = os.stat(file_path).st_size
-			filesToAnalize.append(File(name,file_stats,file_path))
+			filesToAnalize.append(File(file_path,file_stats,name))
+
+print("{0} files detected.".format(len(filesToAnalize)))
+for f in filesToAnalize:
+	print(f.get_path())
 
 # Analisis
 groups = []
@@ -49,7 +51,7 @@ for f in filesToAnalize:
 
 #Resultados
 roPrint = []
-print("{0} grupos".format(len(groups)))
+print("{0} groups.".format(len(groups)))
 with open('posiblesCopias.csv', 'w', newline='') as file:
 	writer = csv.writer(file)
 	for g in groups:
@@ -58,6 +60,6 @@ with open('posiblesCopias.csv', 'w', newline='') as file:
 			toPrint = g.return_files()
 			for p in toPrint:
 				writer.writerow(p)
-			print("Posibles Copias:")		
+			print("Posibles Copias (%% similitud:%.1f):" % (g.get_same_max()))		
 			g.print_files()
 			print("________________________________\n")
